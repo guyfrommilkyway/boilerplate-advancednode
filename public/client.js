386ed320d3dc24ae01a1aca40778ab04901a96a9
Almer Tampus <almerflorestampus@gmail.com>
2024-02-18 04:01:39 +0800

    Listen to users

--- public/client.js
+++ public/client.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
-  /*global io*/
-  let socket = io();
+	/*global io*/
+	let socket = io();
 
-  socket.on('user count', function(data) {
-    console.log(data);
-  });
+	socket.on('user', function (data) {
+		$('#num-users').text(data.currentUsers + ' users online');
+		let message =
+			data.username +
+			(data.connected ? ' has joined the chat.' : ' has left the chat.');
 
-  // Form submittion with new message in field with id 'm'
-  $('form').submit(function () {
-    var messageToSend = $('#m').val();
+		$('#messages').append($('<li>').html('<b>' + message + '</b>'));
+	});
 
-    $('#m').val('');
-    return false; // prevent form submit from refreshing page
-  });
+	// Form submittion with new message in field with id 'm'
+	$('form').submit(function () {
+		var messageToSend = $('#m').val();
+
+		$('#m').val('');
+		return false; // prevent form submit from refreshing page
+	});
 });
