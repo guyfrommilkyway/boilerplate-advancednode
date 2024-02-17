462ec3400c3bf430c0401c8471b25ee185a7f1d5
Almer Tampus <almerflorestampus@gmail.com>
2024-02-18 04:14:21 +0800

    Remove whitespace

--- server.js
+++ server.js
@@ -69,16 +69,12 @@ myDB(async (client) => {
 
 	io.on('connection', (socket) => {
 		++currentUsers;
-
 		io.emit('user count', currentUsers);
-
 		console.log('A user has connected');
 
 		socket.on('disconnect', () => {
 			console.log('A user has disconnected');
-
 			--currentUsers;
-
 			io.emit('user count', currentUsers);
 		});
 	});
